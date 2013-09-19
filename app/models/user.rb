bb7fcd1662fdb92b72a048720c4d81b625736135
TheKnightArgent <github@jsiii.com>
2013-09-18 19:29:44 -0700

    Added password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ActiveRecord::Base
+
+  before_save do
+    self.email = email.downcase
+    self.username = username.downcase
+  end
+
+  has_secure_password
+
   has_many :photos, dependent: :destroy
   validates :email, presence: true, uniqueness: true
   validates :username, presence: true, uniqueness: true
